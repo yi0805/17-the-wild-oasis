cea7b6e2e1c9fc2cf85f65f160a447f15952457e
yi0805ian <ytan960@aucklanduni.ac.nz>
2026-02-14 17:50:01 +1300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+function App() {
+  return (
+    <div>
+      <H1>The Wild Oasis</H1>
+    </div>
+  );
+}
+
+export default App;
