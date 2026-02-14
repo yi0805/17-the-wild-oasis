7411767269138deb456e02c87bfaf91b45dcf7ef
yi0805ian <ytan960@aucklanduni.ac.nz>
2026-02-14 18:41:36 +1300

    Add styled Button and Input components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
 import styled from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
 `;
 
 const StyleApp = styled.div`
