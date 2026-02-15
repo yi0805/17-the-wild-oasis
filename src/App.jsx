b7d9f92fc3466c8f780f1c883abb664d04c9fbe9
yi0805ian <ytan960@aucklanduni.ac.nz>
2026-02-15 14:43:17 +1300

    Add Header and Sidebar components.

--- src/App.jsx
+++ src/App.jsx
@@ -18,17 +18,17 @@ function App() {
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
-
           <Route element={<AppLayout />}>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="*" element={<PageNotFound />} />
+            <Route index element={<Navigate replace to="dashboard" />} />
 
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Account />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
           <Route path="login" element={<Login />} />
         </Routes>
       </BrowserRouter>
