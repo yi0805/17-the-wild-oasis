dd030a03ce7e40e41237699e40b6e699bface014
yi0805ian <ytan960@aucklanduni.ac.nz>
2026-02-18 18:03:54 +1300

    Add useOutsideClick hook and integrate it into Modal component for improved click handling

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react";
+
+export default function useOutsideClick(handler, listenCapture = true) {
+  const ref = useRef();
+
+  useEffect(
+    function () {
+      function handleClick(e) {
+        if (ref.current && !ref.current.contains(e.target)) {
+          handler();
+        }
+      }
+
+      document.addEventListener("click", handleClick, listenCapture);
+
+      return () =>
+        document.removeEventListener("click", handleClick, listenCapture);
+    },
+    [handler, listenCapture],
+  );
+
+  return ref;
+}
