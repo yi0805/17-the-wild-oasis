7ee173a7304e5b2a844bfb21c00f1640cc9f7c53
yi0805ian <ytan960@aucklanduni.ac.nz>
2026-02-21 22:31:38 +1300

    Add DurationChart component to DashboardLayout; implement stay duration summary visualization

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -39,7 +40,8 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       />
       <div>Stas</div>
-      <div>Stas</div>
+
+      <DurationChart confirmedStays={confirmedStays} />
 
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
