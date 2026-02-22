21c315495b0d6148c9fc4d1250cc8b5d44272764
yi0805ian <ytan960@aucklanduni.ac.nz>
2026-02-22 16:12:13 +1300

    Fix import path for useCheckout in CheckoutButton component

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,6 +1,6 @@
 import Button from "../../ui/Button";
 
-import { useCheckout } from "./useCheckOut";
+import { useCheckout } from "./useCheckout";
 
 function CheckoutButton({ bookingId }) {
   const { checkout, isCheckingOut } = useCheckout();
