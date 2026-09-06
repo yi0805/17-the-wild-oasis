83305e9d160fed21bbf9cb5027e7f29e11c74cc2
yi0805ian <ytan960@aucklanduni.ac.nz>
2026-09-06 18:30:31 +1200

    test: add Phase 1 service regression coverage

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
+  },
 });
